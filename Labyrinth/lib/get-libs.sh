eca41ff9575d0e4521df8726559bfcc6c627d2d6
Miroslav <Mirinda.Posp@gmail.com>
2015-05-06 04:56:38 +0200

    get-lib.sh

---
+++ Labyrinth/lib/get-libs.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+wget disk.mirvan.cz/temp/Labyrinth.jpg
+wget disk.mirvan.cz/temp/wayc_false_false_true_true.jpg
+wget disk.mirvan.cz/temp/wayc_false_true_true_false.jpg
+wget disk.mirvan.cz/temp/wayc_true_false_false_true.jpg
+wget disk.mirvan.cz/temp/wayc_true_true_false_false.jpg
+wget disk.mirvan.cz/temp/wayf_false_true_true_true.jpg
+wget disk.mirvan.cz/temp/wayf_true_false_true_true.jpg
+wget disk.mirvan.cz/temp/wayf_true_true_false_true.jpg
+wget disk.mirvan.cz/temp/wayf_true_true_true_false.jpg
+wget disk.mirvan.cz/temp/wayl_false_true_false_true.jpg
+wget disk.mirvan.cz/temp/wayl_true_false_true_false.jpg
+
+mkdir ./treasures
+
+wget disk.mirvan.cz/temp/treasures/treasure1.png -o ./treasures/treasure1.png
+wget disk.mirvan.cz/temp/treasures/treasure2.png -o ./treasures/treasure2.png
+wget disk.mirvan.cz/temp/treasures/treasure3.png -o ./treasures/treasure3.png
+wget disk.mirvan.cz/temp/treasures/treasure4.png -o ./treasures/treasure4.png
+wget disk.mirvan.cz/temp/treasures/treasure5.png -o ./treasures/treasure5.png
+wget disk.mirvan.cz/temp/treasures/treasure6.png -o ./treasures/treasure6.png
+wget disk.mirvan.cz/temp/treasures/treasure7.png -o ./treasures/treasure7.png
+wget disk.mirvan.cz/temp/treasures/treasure8.png -o ./treasures/treasure8.png
+wget disk.mirvan.cz/temp/treasures/treasure9.png -o ./treasures/treasure9.png
+wget disk.mirvan.cz/temp/treasures/treasure10.png -o ./treasures/treasure10.png
+wget disk.mirvan.cz/temp/treasures/treasure11.png -o ./treasures/treasure11.png
+wget disk.mirvan.cz/temp/treasures/treasure12.png -o ./treasures/treasure12.png
+wget disk.mirvan.cz/temp/treasures/treasure13.png -o ./treasures/treasure13.png
+wget disk.mirvan.cz/temp/treasures/treasure14.png -o ./treasures/treasure14.png
+wget disk.mirvan.cz/temp/treasures/treasure15.png -o ./treasures/treasure15.png
+wget disk.mirvan.cz/temp/treasures/treasure16.png -o ./treasures/treasure16.png
+wget disk.mirvan.cz/temp/treasures/treasure17.png -o ./treasures/treasure17.png
+wget disk.mirvan.cz/temp/treasures/treasure18.png -o ./treasures/treasure18.png
+wget disk.mirvan.cz/temp/treasures/treasure19.png -o ./treasures/treasure19.png
+wget disk.mirvan.cz/temp/treasures/treasure20.png -o ./treasures/treasure20.png
+wget disk.mirvan.cz/temp/treasures/treasure21.png -o ./treasures/treasure21.png
+wget disk.mirvan.cz/temp/treasures/treasure22.png -o ./treasures/treasure22.png
+wget disk.mirvan.cz/temp/treasures/treasure23.png -o ./treasures/treasure23.png
+wget disk.mirvan.cz/temp/treasures/treasure24.png -o ./treasures/treasure24.png
+wget disk.mirvan.cz/temp/treasures/treasure25.png -o ./treasures/treasure25.png
+wget disk.mirvan.cz/temp/treasures/treasure26.png -o ./treasures/treasure26.png
+wget disk.mirvan.cz/temp/treasures/treasure27.png -o ./treasures/treasure27.png
+wget disk.mirvan.cz/temp/treasures/treasure28.png -o ./treasures/treasure28.png
+wget disk.mirvan.cz/temp/treasures/treasure29.png -o ./treasures/treasure29.png
+wget disk.mirvan.cz/temp/treasures/treasure30.png -o ./treasures/treasures30.png
+
+mkdir ./players
+
+wget disk.mirvan.cz/temp/players/player_1.png -o ./players/player_1.png
+wget disk.mirvan.cz/temp/players/player_12.png -o ./players/player_12.png
+wget disk.mirvan.cz/temp/players/player_123.png -o ./players/player_123.png
+wget disk.mirvan.cz/temp/players/player_1234.png -o ./players/player_1234.png
+wget disk.mirvan.cz/temp/players/player_124.png -o ./players/player_124.png
+wget disk.mirvan.cz/temp/players/player_13.png -o ./players/player_13.png
+wget disk.mirvan.cz/temp/players/player_134.png -o ./players/player_134.png
+wget disk.mirvan.cz/temp/players/player_14.png -o ./players/player_14.png
+wget disk.mirvan.cz/temp/players/player_2.png -o ./players/player_2.png
+wget disk.mirvan.cz/temp/players/player_23.png -o ./players/player_23.png
+wget disk.mirvan.cz/temp/players/player_234.png -o ./players/player_234.png
+wget disk.mirvan.cz/temp/players/player_24.png -o ./players/player_24.png
+wget disk.mirvan.cz/temp/players/player_3.png -o ./players/player_3.png
+wget disk.mirvan.cz/temp/players/player_34.png -o ./players/player_34.png
+wget disk.mirvan.cz/temp/players/player_4 -o ./players/player_4.png
