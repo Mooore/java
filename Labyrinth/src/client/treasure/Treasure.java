127edcb904b6dadd68e2504150ddb47385f12441
Mafianek <ddressler@seznam.cz>
2015-05-06 04:32:30 +0200

    Treasure javadoc

--- Labyrinth/src/client/treasure/Treasure.java
+++ Labyrinth/src/client/treasure/Treasure.java
@@ -9,12 +9,30 @@ import java.io.Serializable;
  *
  * @author xpospi73, xdress00
  */
+
+/**
+ * This class represents treasures.
+ */
 public class Treasure implements Serializable {
-    
+   
+    /**
+     * Card order.
+     */
     public final int code;
+    /**
+     * Actual count of treasures.
+     */
     public static int cards;
+    /**
+     * Picture of the treasure.
+     */
     public final int picture;
     
+    /**
+     * Constructor sets treasures code and picture.
+     * @param code represents treasure code.
+     * @param pic represents treasure picture.
+     */
     public Treasure(int code, int pic){
         this.code = code;
         this.picture = pic;
